0d7057a0bbe333a309c028cb04d2e886baeb1630
shyam_techy_12 <shyamsundar25291679@gmail.com>
2026-08-24 21:30:44 +0530

    Delete 02.Calculate Age From Birth Year.py

--- 02.Calculate Age From Birth Year.py
+++
@@ -1,7 +0,0 @@
-current_year = int(input('Enter the current year:'))
-birth_year = int(input('Enter your Birth Year:'))
-age = current_year - birth_year
-name = input('Enter your name:')
-print('Hello,', name)
-print('You were born in the year:', birth_year)
-print('Your Age is :', age)
