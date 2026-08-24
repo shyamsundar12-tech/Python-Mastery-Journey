fd6a0b2b75cf7b15ce95101ba1259e28195dc550
shyam_techy_12 <shyamsundar25291679@gmail.com>
2026-08-24 21:31:20 +0530

    Delete 05.Simple Interest.py

--- 05.Simple Interest.py
+++
@@ -1,9 +0,0 @@
-principle = float(input('Enter the principle amount:'))
-rate = float(input('Enter the rate of interest:'))
-tenure = float(input('Enter the tenure in years:'))
-
-simple_interest = (principle * rate * tenure)/100
-print('The simple interest is :', simple_interest)
-
-total_amount = principle + simple_interest
-print('The total amount after', tenure, 'years is :', total_amount)
